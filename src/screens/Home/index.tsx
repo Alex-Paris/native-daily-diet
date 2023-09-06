12386d7937bd978ab2a9050e54c2a47b83c33e66
Alex Paris <alex_paris28@hotmail.com>
2023-09-06 03:22:08 -0300

    chore: meal list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,75 @@
+import { SectionList, View } from "react-native";
+
 import { Block } from "@components/Block";
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 
-import { Container, TopText } from "./styles";
+import { Container, ListItem, ListItemDivider, ListItemDot, ListItemHour, ListItemText, ListSection, ListSectionTitle, TopText } from "./styles";
 
 export function Home() {
+  const DATA = [
+    {
+      title: '12.08.22',
+      data: [
+        {
+          hour: '20:00',
+          meal: 'Pizza',
+          isDiet: false,
+        },
+        {
+          hour: '20:00',
+          meal: 'Salada',
+          isDiet: true,
+        },
+        {
+          hour: '20:00',
+          meal: 'Fruta',
+          isDiet: true,
+        }
+      ]
+    },
+    {
+      title: '11.08.22',
+      data: [
+        {
+          hour: '20:00',
+          meal: 'Frango',
+          isDiet: true,
+        },
+        {
+          hour: '20:00',
+          meal: 'Burger',
+          isDiet: false,
+        },
+        {
+          hour: '20:00',
+          meal: 'Risotto',
+          isDiet: false,
+        }
+      ]
+    },
+    {
+      title: '10.08.22',
+      data: [
+        {
+          hour: '20:00',
+          meal: 'Pizza',
+          isDiet: false,
+        },
+        {
+          hour: '20:00',
+          meal: 'Burger',
+          isDiet: false,
+        },
+        {
+          hour: '20:00',
+          meal: 'Risotto',
+          isDiet: false,
+        }
+      ]
+    },
+  ];
+
   return (
     <Container>
       <Header />
@@ -18,6 +83,32 @@ export function Home() {
 
       <TopText>Refeições</TopText>
       <Button icon="plus" text="Nova refeição" />
+
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item.meal + index}
+
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ gap: 8 }}
+        showsVerticalScrollIndicator={false}
+        
+        renderSectionHeader={({section: {title}}) => (
+          <ListSection>
+            <ListSectionTitle>{title}</ListSectionTitle>
+          </ListSection>
+        )}
+
+        renderSectionFooter={() => <View style={{ height: 32 }} />}
+        
+        renderItem={({item}) => (
+          <ListItem>
+            <ListItemHour>{item.hour}</ListItemHour>
+            <ListItemDivider />
+            <ListItemText>{item.meal}</ListItemText>
+            <ListItemDot type={item.isDiet ? 'POSITIVE' : 'NEGATIVE'} />
+          </ListItem>
+        )}
+      />
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,3 +16,69 @@ export const TopText = styled.Text`
     color: ${theme.COLORS.GRAY_700};
   `};
 `
+
+export const ListSection = styled.View`
+  background-color: ${({ theme }) => theme.COLORS.GRAY_100};
+`
+
+export const ListSectionTitle = styled.Text`
+  margin-bottom: 8px;
+
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.LG}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_700};
+  `};
+`
+
+export const ListItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  gap: 12px;
+  padding: 14px 16px;
+  border-radius: 6px;
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_300};
+`
+
+export const ListItemHour = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XS}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_700};
+  `};
+`
+
+export const ListItemDivider = styled.View`
+  width: 1px;
+  height: 14px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_400};
+`
+
+export const ListItemText = styled.Text`
+  flex: 1;
+
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_600};
+  `};
+`
+
+interface ListItemDotStyleProps {
+  type: 'POSITIVE' | 'NEGATIVE' | 'NEUTRAL'
+}
+
+export const ListItemDot = styled.View<ListItemDotStyleProps>`
+  width: 14px;
+  height: 14px;
+  border-radius: 9999px;
+  background-color: ${({ type, theme }) => (
+    type === 'POSITIVE' ?
+      theme.COLORS.GREEN_MID :
+      type === 'NEGATIVE' ?
+      theme.COLORS.RED_MID
+    :
+      theme.COLORS.GRAY_400
+  )};
+`
